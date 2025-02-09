02341be5ee3d42df61a52de0af9cfabca056f177
KundanBand <kundanband140@gmail.com>
2025-02-09 15:29:32 +0530

    Create create iit2024010_task_1.js

---
+++ section_A/create iit2024010_task_1.js
@@ -0,0 +1,8 @@
+
+document.querySelector("button").addEventListener("click", ()=>{
+    document.querySelector("p").style.border="22px solid black";
+    document.querySelector("p").innerHTML="This is a paragrapt element";
+    document.querySelector("div").innerHTML="This is a 'div' element.";
+    document.querySelector("a").innerHTML+="this is basically a link";
+    document.querySelector("button").style.backgroundColor="red";
+});
