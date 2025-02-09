f86af4af9021f4e26213f7425318c78528b67fdd
CtezNik06 <goyalnikhil744@gmail.com>
2025-02-09 12:30:14 +0530

    Create iit2024020_task1.js

---
+++ section_A/iit2024020_task1.js
@@ -0,0 +1,6 @@
+window.onload=function(){
+    document.querySelector('h1').textContent="hello";
+    document.querySelector('p').textContent="...............";
+    document.querySelector("a").href="https://www.youtube.com";
+    
+}
