fdba5e723f0c157e3b94f53904e2052bb09164a2
The-KrishVerma <krishverma060306@gmail.com>
2025-02-09 21:33:58 +0530

    IIT2024050_task1.js

---
+++ section_A/IIT2024050_task1.js
@@ -0,0 +1,6 @@
+document.querySelector("button").addEventListener("click", ()=>{
+    document.querySelector("div").innerHTML="This is a 'div' element filled using javascript.";
+    document.querySelector("p").style.margin="20px 0px;";
+    document.querySelector("p").innerHTML="This is a 'p' element filled using javascript.";
+    document.querySelector("a").innerHTML+=`This is an 'a' element filled using javascript.`;
+});
