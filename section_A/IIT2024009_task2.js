1bd143e411dc34c30f54a17e0917b787be5d0457
Aditya Sirsalkar <adityasirsalkar33@gmail.com>
2025-02-09 19:09:52 +0530

    Create IIT2024009_task2.js

---
+++ section_A/IIT2024009_task2.js
@@ -0,0 +1,39 @@
+document.body.childNodes[0].innerText ="Web Dev";
+document.body.style.background = "blue";
+console.dir(document.body);
+
+
+let p = document.getElementsByTagName("p");
+console.log(p);
+console.dir(p);
+
+
+let d = document.getElementById("heading");
+console.log(d);
+console.dir(d);
+
+let c = document.getElementsByClassName("aditya");
+console.log(c);
+console.dir(c);
+
+
+let e = document.querySelector(".arsh");
+console.log(e);
+console.dir(e);
+
+
+let element = document.querySelector("li");
+console.log(element);
+console.dir(element);
+
+
+let Allelements = document.querySelectorAll("li");
+console.log(Allelements);
+console.dir(Allelements);
+
+
+let ele = document.querySelector("#heading");
+console.log(ele);
+console.dir(ele);
+
+
