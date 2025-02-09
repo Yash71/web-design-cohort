4401e91aaab4de8b5274a0ea582586ea3d1b2a30
Ajay_Kathar <ajaykathar30@gmail.com>
2025-02-09 15:08:44 +0530

    Create IIT2024037_Task_1.js

---
+++ IIT2024037_Task_1.js
@@ -0,0 +1,31 @@
+function byclass(){
+    let input=document.getElementsByClassName("class")[0]
+    let data=input.value;
+    if(input.value==""){alert("enter text first...");return;}
+    console.log(data);
+    input.value="";
+    let p=document.getElementsByClassName("inputdata")[0]
+    p.innerHTML=data;
+    
+}
+function byid(){
+    let input=document.getElementById("id")
+    let data=input.value
+    if(input.value==""){alert("enter text first...");return;}
+
+    console.log(data);
+    input.value=""
+    let p=document.getElementsByClassName("inputdata")[0]
+    p.innerHTML=data;
+
+}
+function byqs(){
+    let input=document.querySelector(".class")
+    let data=input.value
+    if(input.value==""){alert("enter text first...");return;}
+
+    console.log(data);
+    input.value=""
+    let p=document.getElementsByClassName("inputdata")[0]
+    p.innerHTML=data;
+}
