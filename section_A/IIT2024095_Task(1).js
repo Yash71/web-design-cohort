24b8a5066090f639cf43407a2958ae4617031044
Nitinkr03 <nitinkr03112006@gmail.com>
2025-02-09 15:33:15 +0530

    created IIT2024095_Task(1).js

---
+++ section_A/IIT2024095_Task(1).js
@@ -0,0 +1,6 @@
+let h1=document.querySelector("h1");
+    h1.innerHTML="Hello World";
+    let p=document.querySelector("p");
+    p.innerHTML="lorem lorem ";
+    let a=document.querySelector("a");
+    a.innerHTML="Google";
