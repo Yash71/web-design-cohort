b87c73993da39c3edf82c136707f68c9148927f6
Piyushgarg2000 <ajitgarg546@gmail.com>
2025-02-09 13:14:51 +0530

    Update and rename IIT2024100_task1.js to IIT2024100_task2.js

---
+++ section_A/IIT2024100_task2.js
@@ -0,0 +1,16 @@
+// arrow fn
+const sum=(a,b,c) =>{
+    return a+b+c;
+
+}
+// inbulit fn
+let boxes=document.querySelectorAll("button");
+let box1=boxes[0];
+box1.style.color="red";
+let div1=document.querySelector(".front");
+let div2=document.getElementById("special");
+let paragarph=document.getElementsByClassName("para");
+let para1=paragarph[0];
+para1.style.color="green";
+let para2=paragarph[1];
+para2.style.color="blue";
