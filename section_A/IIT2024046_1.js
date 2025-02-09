63e479923b077315a012817d879a647402b7c5fe
Vardaan Gupta <vardaangupta060@gmail.com>
2025-02-09 21:17:18 +0530

    Add files via upload

---
+++ section_A/IIT2024046_1.js
@@ -0,0 +1,8 @@
+let heading=document.querySelector("h1");
+let paragraph=document.querySelector("p");
+let anchor=document.querySelector("a");
+heading.innerText="BASICS OF JAVASCRIPT";
+paragraph.innerText="JAVASCRIPT IS A SINGLE THREAD LANGUAGE."
+anchor.innerText="CLICK"
+let body=document.querySelector("body");
+body.style.background="green";
