ccbdb029e3887a2a84472a2b9a341e7576b07ce5
adityaagg4689 <184803834+adityaagg4689@users.noreply.github.com>
2025-02-09 13:28:33 +0530

    Created IIT2024101_task2.js

--- section_A/IIT2024101_task1.js
+++
@@ -1,6 +0,0 @@
-let h3 = document.querySelector("h3")
-let p = document.querySelector("p")
-let span = document.querySelector("span")
-h1.textContent = "Hello wrold"
-p.textContent = "Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestias non sunt repellendus, praesentium doloremque quis, magnam amet autem necessitatibus omnis, labore asperiores aperiam quibusdam distinctio ipsa explicabo architecto officia. Ducimus, ad aut"
-span.textContent="lorem ipsum      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quae sequi iure sapiente ex quaerat!"
---
+++ section_A/IIT2024101_task2.js
@@ -0,0 +1,6 @@
+let button = document.querySelector("button")
+button.textContent = "Button"
+let p = document.getElementsByClassName("pa")
+p[0].textContent = "Lorem ipsum dolor sit amet consectetur adipisicing elit. Ex labore iusto optio reprehenderit quis minima rerum. Maxime exercitationem hic nam voluptatem!"
+let div = document.getElementById("q")
+div.textContent = "Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsa, vero."
