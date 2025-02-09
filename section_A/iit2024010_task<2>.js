ceb8066431ced187c942128cd116e36c1a796f7b
KundanBand <kundanband140@gmail.com>
2025-02-09 16:08:10 +0530

    Create iit2024010_task<2>.js

---
+++ section_A/iit2024010_task<2>.js
@@ -0,0 +1,12 @@
+let one=document.getElementsByClassName("o");
+one[0].style.fontSize="22px";
+one[0].style.margin="31px"
+one[1].style.fontSize="22px";
+one[1].style.margin="31px"
+one[2].style.fontSize="22px";
+one[2].style.margin="31px"
+
+document.querySelector("#button").onclick = function(event){
+    this.style.backgroundColor = "green"; 
+}
+
