a62bed09a5b93fa1c431354487a45ef36f2f1f9e
ABHIJEET JAIN <iit2024044@iiita.ac.in>
2025-02-09 16:36:06 +0530

    IIT2024044_task1.js
    solution to task 1

---
+++ section_A/task_1.js
@@ -0,0 +1,6 @@
+let h1 = document.querySelector(".heading")
+let p = document.querySelector(".para")
+let a = document.querySelector(".anchor")
+h1.textContent = "hey this a heading"
+p.textContent = "This is a very wonderful course"
+a.textContent = "click me"
