661727274077c3a599099619481a8b5cb9da9445
Nickel-26 <nickel2608@gmail.com>
2025-02-10 11:33:23 +0530

    Create IIT2024076_task1.js

---
+++ section_A/IIT2024076_task1.js
@@ -0,0 +1,7 @@
+let heading = document.querySelector("h1");
+let head2 = document.querySelector("h2");
+let para = document.querySelector("p");
+
+if (heading) heading.textContent = "This is the biggest heading";
+if (head2) head2.textContent = "This is the second biggest heading";
+if (para) para.textContent = "This is a paragraph";
