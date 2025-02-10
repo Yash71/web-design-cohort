3d9b3ce431624c543cedc3116c7014139d6cfe64
Nickel-26 <nickel2608@gmail.com>
2025-02-10 11:16:19 +0530

    Update IIT2024076_task1.js

--- IIT2024076_task1.js
+++ IIT2024076_task1.js
@@ -1,6 +1,6 @@
 let heading = document.querySelector("h1")
 let head2 = document.querySelector("h2")
-let para = document.querySelector("span")
+let para = document.querySelector("p")
 heading.textContent = "this is the biggest heading"
 head2.textContent = "this is the secong biggest heading"
-span.textContent= "this is a paragraph"
+para.textContent= "this is a paragraph"
