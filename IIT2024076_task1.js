81f43f543d399f2b10fe6ef4382debc27079d9e7
Nickel-26 <nickel2608@gmail.com>
2025-02-10 11:07:53 +0530

    Create IIT2024076_task1.js

---
+++ IIT2024076_task1.js
@@ -0,0 +1,6 @@
+let heading = document.querySelector("h1")
+let head2 = document.querySelector("h2")
+let para = document.querySelector("span")
+heading.textContent = "this is the biggest heading"
+head2.textContent = "this is the secong biggest heading"
+span.textContent= "this is a paragraph"
